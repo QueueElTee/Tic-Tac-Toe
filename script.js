fc80321ab9098f48a0cf18f1843e17b0f17e3a6a
QueueElTee <tijaniqueen17@gmail.com>
2022-09-21 16:13:37 +0100

    Add Form Event Listener

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerForm = document.querySelector('.player-form');
+let player1 = '';
+let player2 = '';
+
 const player = (name, marker) => {
     return { name, marker }
 }
@@ -6,3 +10,13 @@ const gameBoard = (() => {
     let board = [];
     return { board };
 })();
+
+playerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let player1Name = document.querySelector('#player1').value;
+    let player2Name = document.querySelector('#player2').value;
+
+    player1 = player(player1Name, 'X');
+    player2 = player(player2Name, 'O');
+});
